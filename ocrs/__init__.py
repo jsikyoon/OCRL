3d98a7d495e638dc74dbbc9b69a9cf4d7ed0a00f
Jaesik Yoon <jaesik.yoon.kr@gmail.com>
2023-07-07 17:35:38 +0900

    fix a bug on calling OCRs

--- ocrs/__init__.py
+++ ocrs/__init__.py
@@ -1,9 +1,9 @@
 from .gt import GT, GT_Module
 from .iodine import Iodine, Iodine_Module
-from .monet import MoNet, MoNet_Module
+#from .monet import MoNet, MoNet_Module
 from .naturecnn import NatureCNN, NatureCNN_Module
 from .slate import SLATE, SLATE_Module
-from .slotattn import SlotAttn, SlotAttn_Module
+#from .slotattn import SlotAttn, SlotAttn_Module
 from .vaes import VAE, VAE_Module
 #from .vit import ViT, ViT_Module
 from .multiple_cnns import MultipleCNN, MultipleCNN_Module
